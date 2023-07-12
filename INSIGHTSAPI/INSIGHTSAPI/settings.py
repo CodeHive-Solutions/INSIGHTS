60cd94dded8a941e3e8d77a647727101ace4f1f6
sebas <carrenosebastian54@gmail.com>
2023-07-12 14:40:25 -0500

    Cors settings order

--- INSIGHTSAPI/INSIGHTSAPI/settings.py
+++ INSIGHTSAPI/INSIGHTSAPI/settings.py
@@ -58,11 +58,11 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'INSIGHTSAPI.middleware.logging.LoggingMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
