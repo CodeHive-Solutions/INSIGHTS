e3ba372a0fc24fe7feebf86b1d1ae4f29f9ee915
Heibert <heibert203@hotmail.com>
2024-05-07 17:03:06 -0500

    chore: Add QualityNote model and views
    This commit adds the QualityNote model to the quality_notes app, including the user, month, and qualification fields. It also adds the views for the quality_notes app.

--- INSIGHTSAPI/quality_notes/models.py
+++ INSIGHTSAPI/quality_notes/models.py
@@ -1,3 +1,14 @@
+"""Models for the quality_notes app."""
+
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+class QualityNote(models.Model):
+    """Quality note model."""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    month = models.DateField()
+    qualification = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.title
--- INSIGHTSAPI/quality_notes/views.py
+++ INSIGHTSAPI/quality_notes/views.py
@@ -1,1 +1,6 @@
-from rest_framework.viewsets import ModelViewSet
+"""Views for the quality_notes app."""
+
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from quality_notes.models import QualityNote
