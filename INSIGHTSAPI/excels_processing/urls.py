002da726f1ca9a827a053e03c156c4960b6d9ce2
Heibert <heibert203@hotmail.com>
2024-08-21 15:56:25 -0500

    Re activate call_transfers

--- INSIGHTSAPI/excels_processing/urls.py
+++ INSIGHTSAPI/excels_processing/urls.py
@@ -1,8 +1,8 @@
 """This module contains the urls for the excels_processing app."""
 from django.urls import path
-from .views import robinson_list
+from .views import robinson_list, call_transfer_list
 
 urlpatterns = [
     path("robinson-list/", robinson_list, name="robinson-list"),
-    # path("call-transfer-list/", call_transfer_list, name="call-transfer-list"),
+    path("call-transfer-list/", call_transfer_list, name="call-transfer-list"),
 ]
